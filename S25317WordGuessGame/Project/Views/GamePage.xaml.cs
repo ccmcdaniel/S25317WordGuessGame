7496f266971604d2ffdbf6301f3ee50ad7281486
Chad McDaniel <93947281+ccmcdaniel@users.noreply.github.com>
2025-04-02 15:59:15 -0500

    Added a style sheet (AppStyles.xaml) where all custom app styles will be implemented.
    Added base functionality for letter buttons (they can be clicked and highlighted as disabled).

--- S25317WordGuessGame/Project/Views/GamePage.xaml.cs
+++ S25317WordGuessGame/Project/Views/GamePage.xaml.cs
@@ -2,8 +2,63 @@ namespace S25317WordGuessGame.Project.Views;
 
 public partial class GamePage : ContentPage
 {
+	List<Button> letterButtons1 = new List<Button>();
+	List<Button> letterButtons2 = new List<Button>();
+	
 	public GamePage()
 	{
 		InitializeComponent();
+
+		
+
+		string letters1 = "ABCDEFGHIJKLM";
+		string letters2 = "NOPQRSTUVWXYZ";
+
+		SetupLetterButtons(letters1, ref letterButtons1, layoutAlphabetButtons1);
+		SetupLetterButtons(letters2, ref letterButtons2, layoutAlphabetButtons2);
+
 	}
+
+	void SetupLetterButtons(string letters, ref List<Button> buttons, HorizontalStackLayout view)
+	{
+		view.Clear();
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            Button button = MakeLetterButton(letters[i]);
+
+            Border bdr = new();
+            bdr.Style = (Style)Application.Current.Resources["styleLetterButtonBorder"];
+
+            buttons.Add(button);
+            bdr.Content = button;
+
+            view.Add(bdr);
+        }
+    }
+
+
+	private Button MakeLetterButton(char text)
+	{
+		Button btn = new();
+		btn.Style = (Style)Application.Current.Resources["styleLetterButton"];
+		btn.Text = text.ToString();
+		btn.Clicked += LetterButtonClicked;
+
+		return btn;
+    }
+
+    private void LetterButtonClicked(object sender, EventArgs e)
+    {
+		Button b = (Button)sender;
+
+		//Set the button's border to greyed out.
+        Border bdr = (b.Parent as Border);
+		bdr.Style = (Style)Application.Current.Resources["styleLetterButtonBorderDisabled"];
+
+
+        DisplayAlert("Clicked Letter", $"You clicked {b.Text}", "Ok", "Cancel");
+
+		b.IsEnabled = false;
+    }
 }
